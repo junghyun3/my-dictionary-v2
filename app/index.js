1243e4712d2da876d7e10063d04123310f0d85b7
junghyun <junghyun.kwon3@gmail.com>
2020-09-30 18:12:50 +0900

    initial version

---
+++ app/index.js
@@ -0,0 +1,41 @@
+const {
+    app,
+    BrowserWindow
+  } = require('electron')
+  const path = require('path')
+  const isDev = require('electron-is-dev')
+  
+  let mainWindow
+  
+  const createWindow = () => {
+    mainWindow = new BrowserWindow({
+      width: 350,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+  
+    if (isDev) {
+      // Open the DevTools.
+      // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>')
+      // mainWindow.webContents.openDevTools()
+    }
+    mainWindow.on('closed', () => {
+      mainWindow = null
+    })
+  }
+  
+  app.on('ready', createWindow)
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow()
+    }
+  })
+  
